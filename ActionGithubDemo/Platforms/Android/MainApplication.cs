4d74e767e07591a78f979e60015e4ed608778247
Tuyen Vu <tuyen_u@trigent.com>
2022-12-22 10:29:46 +0700

    init

---
+++ ActionGithubDemo/Platforms/Android/MainApplication.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Runtime;
+using Firebase;
+
+namespace ActionGithubDemo;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
+    }
+
+    protected override MauiApp CreateMauiApp()
+    {
+        FirebaseApp.InitializeApp(this);
+        return MauiProgram.CreateMauiApp();
+    }
+}
+
